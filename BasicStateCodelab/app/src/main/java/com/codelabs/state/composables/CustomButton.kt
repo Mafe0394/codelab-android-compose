9b27c70908597d75910edc0eac4cf04d9963bbf9
Mafe Vargas <maria.vargas+meli@mercadolibre.com.co>
2025-05-17 18:56:12 -0500

    Removing imports

--- BasicStateCodelab/app/src/main/java/com/codelabs/state/composables/CustomButton.kt
+++ BasicStateCodelab/app/src/main/java/com/codelabs/state/composables/CustomButton.kt
@@ -1,11 +1,9 @@
 package com.codelabs.state.composables
 
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 
 @Composable
 fun NormalButton(
--- BasicStateCodelab/app/src/main/java/com/codelabs/state/composables/WellnessTaskItem.kt
+++ BasicStateCodelab/app/src/main/java/com/codelabs/state/composables/WellnessTaskItem.kt
@@ -1,6 +1,5 @@
 package com.codelabs.state.composables
 
-import android.R
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
@@ -9,15 +8,9 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.codelabs.state.ui.theme.BasicStateCodelabTheme
 
 @Composable
 fun WellnessTaskItem(
